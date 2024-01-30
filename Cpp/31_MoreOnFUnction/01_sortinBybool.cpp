85d60139db9eb2e242d1f9762c3e97e33be465a8
Noman Ahmad <mdnoman70499@gmail.com>
2024-01-30 23:57:34 +0530

    questions added

---
+++ Cpp/31_MoreOnFUnction/01_sortinBybool.cpp
@@ -0,0 +1,3 @@
+// 1. Define a function to sort an array of integers in ascending or descending order 
+// depending on bool type argument (true for ascending and false for descending)
+// use default argument to implement it.
---
+++ Cpp/31_MoreOnFUnction/05_PrimeFactor.cpp
@@ -0,0 +1,10 @@
+// 5. Define a function to print all the prime factors of a given number. 
+// [for example. num=36 , prime factors are 2,3]
+
+#include<iostream>
+using namespace std;
+int main()
+{
+
+return 0 ;
+}
