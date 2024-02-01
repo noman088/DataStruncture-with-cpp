24b99c7f639ef01b89bb4f2de0f272200979196d
Noman Ahmad <mdnoman70499@gmail.com>
2024-02-01 23:50:40 +0530

    only questoin 2 added

---
+++ Cpp/31_MoreOnFUnction/02_SortArrayOfString.cpp
@@ -0,0 +1,3 @@
+// 2. Define a function to sort an array of strings in ascending or descending order 
+// depending on bool type argument (true for ascending and false for descending)
+// Use default argument to implement it.
