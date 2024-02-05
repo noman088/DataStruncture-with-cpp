15ffc1147a0dd88bd57fdd5d60363bbb8df65b01
Noman Ahmad <mdnoman70499@gmail.com>
2024-02-05 23:52:27 +0530

    delete some unnecessory file

---
+++ Cpp/31_MoreOnFUnction/03_Rotation.cpp
@@ -0,0 +1,16 @@
+/*
+
+3. Define a function to rotate an array by n positions in the d direction. implement
+position and direction using default arguments. Argument d can be -1 or 1 denoting
+left or right direction. Argument n should be the last argument with default value 1.
+
+*/
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+
+return 0 ;
+}
