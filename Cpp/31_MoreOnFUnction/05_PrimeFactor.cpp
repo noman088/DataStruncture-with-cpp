cf541bed6c5d2e1ef3af4e4e513279a9f60f1e33
Noman Ahmad <mdnoman70499@gmail.com>
2024-02-12 22:53:35 +0530

    hcf done q6

--- Cpp/31_MoreOnFUnction/05_PrimeFactor.cpp
+++ Cpp/31_MoreOnFUnction/05_PrimeFactor.cpp
@@ -1,10 +1,37 @@
-// 5. Define a function to print all the prime factors of a given number. 
+// 5. Define a function to print all the prime factors of a given number.
 // [for example. num=36 , prime factors are 2,3]
 
-#include<iostream>
+#include <iostream>
 using namespace std;
+int checkPrime(int n)
+{
+        for (int i = 2; i < n / 2; i++)
+        {
+                if (n % i == 0)
+                {
+                        return 0;
+                }
+        }
+        return 1;
+}
+void PrintPrimes(int n)
+{
+        for (int i = 2; i < n; i++)
+        {
+                if (n % i == 0)
+                {
+
+                        if (checkPrime(i) == 1)
+                                cout << " " << i;
+                }
+        }
+}
 int main()
 {
+        // int n;
+        // cin >> n;
+        int n = 36;
+        PrintPrimes(n);
 
-return 0 ;
+        return 0;
 }
