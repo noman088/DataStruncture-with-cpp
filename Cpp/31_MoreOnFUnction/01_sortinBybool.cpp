f8e63ac15940ce5e99fd4cdf2be6fdebcd63f306
Noman Ahmad <mdnoman70499@gmail.com>
2024-02-24 20:26:10 +0530

    Questoin 1st solved

--- Cpp/31_MoreOnFUnction/01_sortinBybool.cpp
+++ Cpp/31_MoreOnFUnction/01_sortinBybool.cpp
@@ -3,8 +3,44 @@
 // use default argument to implement it.
 #include <iostream>
 using namespace std;
+void sortarr(int a[], bool x)
+{
+        if (x == true)
+        {
+
+                for (int i = 0; i < 5; i++)
+                        for (int j = i + 1; j < 5; j++)
+                                if (a[i] > a[j])
+                                {
+                                        int x = a[i];
+                                        a[i] = a[j];
+                                        a[j] = x;
+                                }
+                for (int i = 0; i < 5; i++)
+                        cout << a[i] << " ";
+        }
+        cout << endl;
+        if (x == false)
+        {
+
+                for (int i = 0; i < 5; i++)
+                        for (int j = i + 1; j < 5; j++)
+                                if (a[i] < a[j])
+                                {
+                                        int x = a[i];
+                                        a[i] = a[j];
+                                        a[j] = x;
+                                }
+                for (int i = 0; i < 5; i++)
+                        cout << a[i] << " ";
+        }
+        cout << endl;
+}
 int main()
 {
+        int a[] = {1, 4, 2, 5, 3};
+        sortarr(a, true);
+        sortarr(a, false);
 
         return 0;
 }
