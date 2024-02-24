22065a6c2bc7e46ede7ffccaf28e910126e41a8a
Noman Ahmad <mdnoman70499@gmail.com>
2024-02-24 07:22:48 +0530

    cleaning

--- Cpp/31_MoreOnFUnction/01_sortinBybool.cpp
+++ Cpp/31_MoreOnFUnction/01_sortinBybool.cpp
@@ -1,3 +1,10 @@
-// 1. Define a function to sort an array of integers in ascending or descending order 
+// 1. Define a function to sort an array of integers in ascending or descending order
 // depending on bool type argument (true for ascending and false for descending)
 // use default argument to implement it.
+#include <iostream>
+using namespace std;
+int main()
+{
+
+        return 0;
+}
