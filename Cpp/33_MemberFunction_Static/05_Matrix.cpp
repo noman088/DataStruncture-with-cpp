010f3a85e16e5ba30b19082fbc325f581ac8ac40
Noman Ahmad <mdnoman70499@gmail.com>
2024-09-10 15:28:33 +0530

    just add quetion to the file

---
+++ Cpp/33_MemberFunction_Static/05_Matrix.cpp
@@ -0,0 +1,9 @@
+
+// 5. Define a class Matrix to represent a 3X3 order matrix. Provide appropriate methods
+// and properies to the class. Also define following methods in the class:
+//     1.Matrix add(Matrix)
+//     2.Matrix sub(Matrix)
+//     3.Matrix multiply(Matrix)
+//     4.Matrix transpose()
+//     5.bool is_singular()
+
