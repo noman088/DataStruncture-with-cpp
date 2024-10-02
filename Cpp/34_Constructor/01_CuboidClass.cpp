334a52c06b553a807c37919196cdc30ac489eb3b
Noman Ahmad <mdnoman70499@gmail.com>
2024-10-03 01:09:03 +0530

    First Question added

--- Cpp/34_Constructor/01_CuboidClass.cpp
+++ Cpp/34_Constructor/01_CuboidClass.cpp
@@ -7,5 +7,19 @@ private:
         int length, breath, height;
 
 public:
-        Cuboid(){};
+        Cuboid(int a, int b, int c)
+        {
+
+                length = a;
+                breath = b;
+                height = c;
+        };
 };
+#include <iostream>
+using namespace std;
+int main()
+{
+        Cuboid a(1, 2, 3);
+
+        return 0;
+}
--- Cpp/34_Constructor/02_CostomerClass.cpp
+++ Cpp/34_Constructor/02_CostomerClass.cpp
@@ -1,2 +1,2 @@
 // 2. Define a class Customer with instance members cust_id , name , email , mobile
-// Define non parametaerised constructor and parameterised constructor in the class. 
+// Define non parametaerised constructor and parameterised constructor in the class.
