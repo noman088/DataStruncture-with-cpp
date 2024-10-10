07d59174a43b172b4e9113e123f8178037901816
Noman Ahmad <mdnoman70499@gmail.com>
2024-10-11 03:51:54 +0530

    Q2 solved

--- Cpp/34_Constructor/02_CostomerClass.cpp
+++ Cpp/34_Constructor/02_CostomerClass.cpp
@@ -1,2 +1,19 @@
 // 2. Define a class Customer with instance members cust_id , name , email , mobile
 // Define non parametaerised constructor and parameterised constructor in the class.
+#include <string.h>
+class Customer
+{
+private:
+        int cust_id;
+        char name[20];
+        int mobile;
+
+public:
+        Customer(int i, char ar[], int m)
+        {
+                cust_id = i;
+                strcpy(name, ar);
+                mobile = m;
+        }
+        Customer() {};
+};
