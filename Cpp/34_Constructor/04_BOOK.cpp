f5c91ac32946d0468836b92c618fce74126a9ee4
Noman Ahmad <mdnoman70499@gmail.com>
2024-10-11 08:53:12 +0530

    Q4 solved

--- Cpp/34_Constructor/04_BOOK.cpp
+++ Cpp/34_Constructor/04_BOOK.cpp
@@ -1,2 +1,19 @@
-// 4. Define a class Book with booked , title and price as instance member variables 
-// Define non parameterrised and parametaerised constructor in the class. 
+// 4. Define a class Book with booked , title and price as instance member variables
+// Define non parameterrised and parametaerised constructor in the class.
+#include <string.h>
+class Book
+{
+private:
+        int bookid;
+        char title[20];
+        float price;
+
+public:
+        Book() {};
+        Book(int bid, char t[], float p)
+        {
+                bookid = bid;
+                strcpy(title, t);
+                price = p;
+        }
+};
