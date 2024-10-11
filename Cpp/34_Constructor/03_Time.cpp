525ef7a52cc20b96c570ca62ce0cc3d59eb0fc71
Noman Ahmad <mdnoman70499@gmail.com>
2024-10-11 08:49:28 +0530

    Q3 done

--- Cpp/34_Constructor/03_Time.cpp
+++ Cpp/34_Constructor/03_Time.cpp
@@ -1,2 +1,15 @@
 // 3. Define a class Time with hr, min, sec as instance member variables . Define
 // constructor to initialise time Objects.
+class Time
+{
+private:
+        int hr, min, sec;
+
+public:
+        Time(int h, int m, int s)
+        {
+                hr = h;
+                min = m;
+                sec = s;
+        }
+};
