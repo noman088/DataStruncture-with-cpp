a8a8514d997f329a2705c499fb5890f4ee74d24a
Noman Ahmad <mdnoman70499@gmail.com>
2024-12-31 05:11:47 +0530

    Q6 solved

--- Cpp/34_Constructor/06_NumberClass.cpp
+++ Cpp/34_Constructor/06_NumberClass.cpp
@@ -1,4 +1,51 @@
 // 6. Define a class Number with size and arr pointer as instance variables. Provide
-// constructor to  initialise instance members, dynamically allocate an array of given 
-// size and store address in arr variable of object. Also define destructor to deallocate 
+// constructor to  initialise instance members, dynamically allocate an array of given
+// size and store address in arr variable of object. Also define destructor to deallocate
 // memory of array . Implement deep copy using copy constructor to avoid memory issue.
+#include <iostream>
+using namespace std;
+class Number
+{
+private:
+        int size;
+        int *arr;
+
+public:
+        Number(int s)
+        {
+                size = s;
+                arr = new int[s];
+                for (int i = 0; i < size; i++)
+                {
+                        arr[i] = i + 1;
+                }
+        }
+        Number(Number &copy)
+        {
+                size = copy.size;
+                arr = new int[size];
+                for (int i = 0; i < size; i++)
+                {
+                        arr[i] = copy.arr[i];
+                }
+        }
+        ~Number()
+        {
+                delete[] arr;
+        }
+        void display()
+        {
+                for (int i = 0; i < size; i++)
+                {
+                        cout << arr[i] << "";
+                }
+                cout << endl;
+        }
+};
+int main()
+{
+        Number a(5);
+        Number b = a;
+        a.display();
+        b.display();
+}
