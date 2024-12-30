2ed759f88abccdc0f51feefc799f0676b8fc7927
Noman Ahmad <mdnoman70499@gmail.com>
2024-12-31 05:18:03 +0530

    Q7 solved

--- Cpp/34_Constructor/07_Student.cpp
+++ Cpp/34_Constructor/07_Student.cpp
@@ -1,2 +1,27 @@
 // 7.Define a class student and write a program to enter student details using
 // constructor and define member fucntion to display all the details.
+#include <string>
+#include <iostream>
+using namespace std;
+class student
+{
+private:
+        int rollNumber;
+        string name;
+
+public:
+        student(int rn, string n)
+        {
+                rollNumber = rn;
+                name = n;
+        };
+        void display()
+        {
+                cout << "Roll Number => " << rollNumber << " " << " Name - " << name << endl;
+        }
+};
+int main()
+{
+        student s(1, "Noman");
+        s.display();
+}
