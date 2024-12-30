7a4e511f790e0ff67bba7e1bc0a10f073d43e96b
Noman Ahmad <mdnoman70499@gmail.com>
2024-12-31 05:24:24 +0530

    Q8 Solved

--- Cpp/34_Constructor/08_Date.cpp
+++ Cpp/34_Constructor/08_Date.cpp
@@ -1,2 +1,24 @@
-// 8. Define a class Date with d, m , y as instance variables. initialise members using 
+// 8. Define a class Date with d, m , y as instance variables. initialise members using
 // initialisers.
+#include <iostream>
+using namespace std;
+class Date
+{
+private:
+        int d, m, y;
+
+public:
+        Date(int date, int month, int year) : d(date), m(month), y(year) {};
+        void display()
+        {
+                cout << "date => " << d << "/" << m << "/" << y << endl;
+        }
+};
+
+// int main()
+// {
+//         Date a(2, 3, 2014);
+//         a.display();
+
+//         return 0;
+// }
