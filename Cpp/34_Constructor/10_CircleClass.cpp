864909199a55ed39ba7aed00c9c4f96530357129
Noman Ahmad <mdnoman70499@gmail.com>
2024-12-31 20:09:02 +0530

    Q10 Solved

--- Cpp/34_Constructor/10_CircleClass.cpp
+++ Cpp/34_Constructor/10_CircleClass.cpp
@@ -1,3 +1,13 @@
 // 10. Define Circle class with radius as instance variable. Define two constructors in the
 // class-non parameterised and parameterised.
+class Circle
+{
+private:
+        int radius;
 
+public:
+        Circle() {};
+        void setRadius(int r) { radius = r; }
+        Circle(int r) { radius = r; }
+        // we can make  area and other function for this class
+};
